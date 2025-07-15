fb5b3a4dcdb52804abfdf20b51d4865261c5a3a9
Luiza <luizabordignonmoro@gmail.com>
2025-07-15 13:29:14 -0300

    12

--- routes/cliente.py
+++ routes/cliente.py
@@ -41,7 +41,8 @@ def form_cliente():
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
     #detalhes sobre o cliente
-    return render_template('detalhe_cliente.html')
+    cliente = list(filter(lambda c: c['id']== cliente_id, clientes))[0]
+    return render_template('detalhe_cliente.html', cliente=cliente)
 
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
@@ -56,8 +57,18 @@ def form_edit_cliente(cliente_id):
 @cliente_route.route('/<int:cliente_id>/update', methods =['PUT'])
 def atualizar_cliente(cliente_id):
     #atualizar informaçoes sobre o cliente
-    pass
+    cliente_editado = None
+    #obter dados do formulario de edição
+    data = request.json
+    #obter o usuario pelo id
+    for c in clientes:
+        if c['id'] == cliente_id:
+            c['nome'] = data['nome']
+            c['email'] = data['email']
 
+            cliente_editado = c 
+    #editar o usuario
+    return render_template('item_cliente.html', cliente=cliente_editado)
 
 @cliente_route.route('/<int:cliente_id>/delete', methods =['DELETE'])
 def deletar_cliente(cliente_id):
